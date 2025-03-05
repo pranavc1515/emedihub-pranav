fbb67b2e736512b0988603ecdaab3a8dd0c899be
Prathamesh <prathameshkarbele101@gmail.com>
2025-03-05 07:23:56 +0530

    wip1

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = ({ children }) => {
   return (
     <div className=" bg-gradient-to-b from-blue-100 to-white min-h-screen w-full">
       {/* Header */}
-      <header className="flex justify-between items-center p-6   bg-gradient-to-t from-blue-100 to-white  z-[999]  w-full">
+      <header className="flex justify-between items-center p-6   bg-gradient-to-t from-blue-100 to-white  w-full">
         <img src={Medihub} alt='midihub' className="w-[150] h-[70px]"/>
         <div className="md:hidden" onClick={toggleMenu}>
         <img src={hambuger_Menu} alt='hambuger_Menu' className="w-[50px] h-[50px]   flex items-center justify-center" />
